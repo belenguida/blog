a50050f18709bf8d1249f32f1e37cee47dce662f
Belen <belen.guida@gmail.com>
2018-08-27 12:58:01 -0300

    Opción Destroy item

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,6 +36,13 @@ def index
   @articles = Article.all
 end
 
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+
+  redirect_to articles_path
+end
+
 private
   def article_params
     params.require(:article).permit(:title, :text)
