18b9e96930d490225a00528641f82c5ad9fb94ed
Belen <belen.guida@gmail.com>
2018-08-27 12:08:50 -0300

    Opción Editar Post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,20 @@ def show
    @article = Article.find(params[:id])
 end
 
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 def index
   @articles = Article.all
 end
